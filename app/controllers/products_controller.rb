e5609c9896f97bfabdc1e6c0658241da9d7dc530
laibaa mirislam <amirlaiba546@gmail.com>
2026-07-30 18:51:40 +0500

    Add create action with strong parameters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,4 +13,20 @@ class ProductsController < ApplicationController
         @product = Product.new
     end
 
+    def create
+        @product = Product.new(product_params)
+
+            if @product.save
+                flash[:notice] = "Product was added successfully."
+                redirect_to product_path(@product)
+            else
+                render 'new'
+            end
+        end
+
+        private
+
+        def product_params
+            params.require(:product).permit(:name, :description, :price, :stock_quantity, :sku)
+        end
 end
