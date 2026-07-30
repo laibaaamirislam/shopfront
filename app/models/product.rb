5ef2722d43a5b1246a503a4abae71df5bd2ea9e9
laibaa mirislam <amirlaiba546@gmail.com>
2026-07-30 16:45:25 +0500

    Add validatios to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :sku, presence: true, uniqueness: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :stock_quantity, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
