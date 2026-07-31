13b24b06c33986810c2bd8b225f3426d2f801444
laibaa mirislam <amirlaiba546@gmail.com>
2026-07-31 14:55:27 +0500

    Add dedicated pages controller and update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,5 +15,7 @@ Rails.application.routes.draw do
 
   # config/routes.rb
   resources :products
-  root 'products#index'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
