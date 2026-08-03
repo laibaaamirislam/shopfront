1f52112ede7343c4551f61afd795ef0e2ccd484b
laibaa mirislam <amirlaiba546@gmail.com>
2026-08-03 15:51:44 +0500

    Style flash and validation messages as Bootstrap alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      "notice" => "alert-success",
+      "alert"  => "alert-danger",
+      "error"  => "alert-danger"
+    }.fetch(flash_type.to_s, "alert-info")
+  end
 end
