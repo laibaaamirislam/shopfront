0b77f346f19cd703d99194530bd6202481b07f99
laibaa mirislam <amirlaiba546@gmail.com>
2026-08-05 13:44:02 +0500

    Add password_digest column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_05_083629) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_05_084311) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_05_083629) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 
