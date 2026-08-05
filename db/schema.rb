002877b9cf3d245cb18048397ee82dfb63b70493
laibaa mirislam <amirlaiba546@gmail.com>
2026-08-05 13:59:26 +0500

    Add email validations to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_05_084311) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_05_085832) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_05_084311) do
     t.string "email"
     t.string "password_digest"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
