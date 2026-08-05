511d94e61302365899cbb7b22982153721694357
laibaa mirislam <amirlaiba546@gmail.com>
2026-08-05 14:07:34 +0500

    Add login/logout via SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,9 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   resources :users, only: [:new, :create]
-  
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
 end
