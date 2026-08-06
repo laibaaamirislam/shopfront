3a8f4ef97cf359b6d8d33179981070b7ce40684c
laibaa mirislam <amirlaiba546@gmail.com>
2026-08-06 16:47:29 +0500

    Add status validation with defined set of allowed statuses

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
+
+  STATUSES = %w[paid shipped delivered cancelled].freeze
+  validates :status, inclusion: { in: STATUSES }
 end
