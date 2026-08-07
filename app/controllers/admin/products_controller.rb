75c053c842c908c44b4ebb06493da95f30ee58df
laibaa mirislam <amirlaiba546@gmail.com>
2026-08-07 14:35:48 +0500

    Add Admin::ProductsController with table-based management UI

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,52 @@
+# app/controllers/admin/products_controller.rb
+module Admin
+  class ProductsController < BaseController
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @products = Product.includes(:category).order(:name)
+    end
+
+    def show
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        redirect_to admin_product_path(@product), notice: "Product created."
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @product.update(product_params)
+        redirect_to admin_product_path(@product), notice: "Product updated."
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product.destroy
+      redirect_to admin_products_path, notice: "Product deleted."
+    end
+
+    private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :stock_quantity, :sku, :category_id, :image)
+    end
+  end
+end
