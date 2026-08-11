bd72aa1968529bf0ee52b26c367f6ece144b9f08
laibaa mirislam <amirlaiba546@gmail.com>
2026-08-11 13:19:05 +0500

    Render the order partial in show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 
 class UsersController < ApplicationController
-  before_action :require_login, only: [:edit, :update]
+  before_action :require_login, only: [:edit, :update, :show]
 
   def new
     @user = User.new
